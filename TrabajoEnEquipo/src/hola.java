ca71013ed79b1420e128f7a844ab69228470c4bf
Daniel <danigonzalloret@gmail.com>
2021-04-15 11:51:06 +0200

    Comenzado
    Signed-off-by: Daniel <danigonzalloret@gmail.com>

--- TrabajoEnEquipo/src/hola.java
+++ TrabajoEnEquipo/src/hola.java
@@ -1,4 +1,9 @@
 
-public class hola {
+import java.util.Scanner;
 
+public class hola {
+	public static Scanner tec = new Scanner(System.in);
+	public static void main(String[] args) {
+		System.out.println("Hola");
+	}
 }
